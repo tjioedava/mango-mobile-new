33fd360d4994cd3cee944ea9cf8e182eb06c89a3
tjioedava <tjioedava@gmail.com>
2024-12-10 17:31:54 +0700

    implemented switch to toggle own product filter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:mango_mobile/screens/login.dart';
+import 'package:mango_mobile/screens/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
                 seedColor: const Color.fromARGB(255, 50, 78, 189)),
             useMaterial3: true,
           ),
-          home: const LoginPage(),
+          home: const ProductPage(),
         ));
   }
 }
--- lib/screens/product_list.dart
+++ lib/screens/product_list.dart
@@ -4,16 +4,34 @@ import 'package:provider/provider.dart';
 import 'package:mango_mobile/widgets/left_drawer.dart';
 import 'package:mango_mobile/models/product.dart';
 
-class ProductPage extends StatefulWidget {
+class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
   @override
-  State<ProductPage> createState() => _ProductPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product List'),
+      ),
+      drawer: const LeftDrawer(),
+      body: ProductSection(),
+    );
+  }
 }
 
-class _ProductPageState extends State<ProductPage> {
+class ProductSection extends StatefulWidget {
+  const ProductSection({super.key});
+
+  @override
+  State<ProductSection> createState() => _ProductSectionState();
+}
+
+class _ProductSectionState extends State<ProductSection> {
+  var _filterOwn = false;
+
   Future<List<Product>> fetchProduct(CookieRequest request) async {
-    final response = await request.get('http://127.0.0.1:8000/show-products');
+    final response = await request
+        .get('http://127.0.0.1:8000/show-products?own=$_filterOwn');
 
     // Melakukan decode response menjadi bentuk json
     var data = response;
@@ -31,63 +49,132 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product List'),
-      ),
-      drawer: const LeftDrawer(),
-      body: FutureBuilder(
-        future: fetchProduct(request),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            if (!snapshot.hasData) {
-              return const Column(
-                children: [
-                  Text(
-                    'This user has no product yet.',
-                    style: TextStyle(fontSize: 20, color: Color(0xff59A5D8)),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              );
+    return Column(children: [
+      Column(children: [
+        const Text("Filter to owned products"),
+        Switch(
+            value: _filterOwn,
+            onChanged: (value) {
+              setState(() {
+                _filterOwn = value;
+              });
+            }),
+      ]),
+      Expanded(
+        child: FutureBuilder(
+          future: fetchProduct(request),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
             } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (_, index) => Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${snapshot.data![index].fields.name}",
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+              if (!snapshot.hasData) {
+                return const Column(
+                  children: [
+                    Text(
+                      'This user has no product yet.',
+                      style: TextStyle(fontSize: 20, color: Color(0xff59A5D8)),
+                    ),
+                    SizedBox(height: 8),
+                  ],
+                );
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (_, index) => Card(
+                    elevation: 4, // Elevation for the shadow effect
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10), // Rounded corners
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(
+                          16.0), // Inner padding for the card
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${snapshot.data![index].fields.name}",
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            children: [
+                              const Icon(Icons.location_on,
+                                  size: 14, color: Colors.grey),
+                              const SizedBox(width: 4),
+                              Expanded(
+                                child: Text(
+                                  "Price: ${snapshot.data![index].fields.price}",
+                                  style: const TextStyle(
+                                      fontSize: 12.0, color: Colors.grey),
+                                  overflow: TextOverflow
+                                      .ellipsis, // Handles long text
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            children: [
+                              const Icon(Icons.book,
+                                  size: 14, color: Colors.grey),
+                              const SizedBox(width: 4),
+                              Expanded(
+                                child: Text(
+                                  "Description: ${snapshot.data![index].fields.description}",
+                                  style: const TextStyle(
+                                      fontSize: 12.0, color: Colors.grey),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            children: [
+                              const Icon(Icons.shopping_bag,
+                                  size: 14, color: Colors.grey),
+                              const SizedBox(width: 4),
+                              Expanded(
+                                child: Text(
+                                  "Items Purchased: ${snapshot.data![index].fields.itemsPurchased}",
+                                  style: const TextStyle(
+                                      fontSize: 12.0, color: Colors.blue),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            children: [
+                              const Icon(Icons.star,
+                                  size: 14, color: Colors.grey),
+                              const SizedBox(width: 4),
+                              Expanded(
+                                child: Text(
+                                  "Ratings: ${snapshot.data![index].fields.rating}",
+                                  style: const TextStyle(
+                                      fontSize: 12.0, color: Colors.blue),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 10),
-                      Text("Price: ${snapshot.data![index].fields.price}"),
-                      const SizedBox(height: 10),
-                      Text(
-                          "Description: ${snapshot.data![index].fields.description}"),
-                      const SizedBox(height: 10),
-                      Text(
-                          "Items Purchased: ${snapshot.data![index].fields.itemsPurchased}"),
-                      const SizedBox(height: 10),
-                      Text("Rating: ${snapshot.data![index].fields.rating}")
-                    ],
+                    ),
                   ),
-                ),
-              );
+                );
+              }
             }
-          }
-        },
+          },
+        ),
       ),
-    );
+    ]);
   }
 }
